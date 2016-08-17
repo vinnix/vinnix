63bdbf99a47a49da6f7b3845fc04b77ac09a106c
Vinnix Schmidt <vinnix.bsd@gmail.com>
2016-08-17 15:47:39 -0300

    Making it more real

--- Factorial/factorial_perl.pl
+++ Factorial/factorial_perl.pl
@@ -6,45 +6,37 @@
 #
 
 
-my $n = 5;
+
+
+# We want to do something like:  echo 55 |  ./factorial_perl.pl
+chomp(my $n = <STDIN>);;
 my $result_to_print = 0;
 
 
 ## I have no problem with a helper function having more arguments.
-sub fact {
+sub fact1 {
    my ($n) = @_;
 
-   local *_fact = sub {
+   local *_fact1 = sub {
        my ($n, $prod) = @_;
        return $prod if $n == 0;
-       return _fact($n-1, $n*$prod);
+       return _fact1($n-1, $n*$prod);
    };
 
-   return _fact($n, 1);
+   return _fact1($n, 1);
 }
 
-$result_to_print  = fact($n);
-print "Resultado $result_to_print \n";
-
-##In this case, you could even fake it as follows:
-sub fact {
-   my ($n, $prod) = @_;
-   $prod ||= 1;
-   return $prod if $n == 0;
-   return _fact($n-1, $n*$prod);
-}
-
-$result_to_print  = fact($n);
+$result_to_print  = fact1($n);
 print "Resultado $result_to_print \n";
 
 
 ## But since Perl doesn't do tail recursion optimisation, and since sub calls are relatively slow, you're better off with:
-sub fact {
+sub fact2 {
    my ($n) = @_;
    my $prod = 1;
    $prod *= $n-- while $n > 0;
    return $prod;
 }
 
-$result_to_print  = fact($n);
+$result_to_print  = fact2($n);
 print "Resultado $result_to_print \n";
