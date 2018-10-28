4159f6b8b62c86ed141b468f4017fc508eb60d4e
Vinícius Schmidt <vinicius.schmidt@gmail.com>
2018-10-28 08:33:59 -0300

    updating make file plus packages analysis

--- FreeBSD/usr/home/vinnix/bin/run.sh
+++ FreeBSD/usr/home/vinnix/bin/run.sh
@@ -4,6 +4,7 @@
 ./pkgs_analysis.sh -a > analysis.autoremove.output
 ./pkgs_analysis.sh -l > analysis.leaf.output
 
+pkg info > pkg.info.output
 
 cat analysis.autoremove.output | grep " 0 dep" | grep "0 that" | awk '{print $4}' | sort | uniq
 cat analysis.leaf.output | grep " 0 dep" | grep "0 that" | awk '{print $4}' | sort | uniq
