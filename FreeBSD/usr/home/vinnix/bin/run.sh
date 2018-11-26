7a90d056f05f49fdf92e6bea507c0edc74d804b7
Vinícius Schmidt <vinicius.schmidt@gmail.com>
2018-11-26 09:24:09 -0200

    Unmaintained + updates after elastisearch/kibana

--- FreeBSD/usr/home/vinnix/bin/run.sh
+++ FreeBSD/usr/home/vinnix/bin/run.sh
@@ -6,6 +6,8 @@
 
 pkg info > pkg.info.output
 
+pkg query -e '%m = ports@FreeBSD.org' %o > pkg.unmaintained.output
+
 cat analysis.autoremove.output | grep " 0 dep" | grep "0 that" | awk '{print $4}' | sort | uniq
 cat analysis.leaf.output | grep " 0 dep" | grep "0 that" | awk '{print $4}' | sort | uniq
 
