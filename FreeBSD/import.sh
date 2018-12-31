11de0f1bbbf88e21eefeac912aaf5c9e2d2313c0
Vinícius Schmidt <vinicius.schmidt@gmail.com>
2018-12-31 11:47:01 -0200

    Trying to make a script to sycning conf. files in a easier way

--- FreeBSD/import.sh
+++ FreeBSD/import.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash
 
+function is_equal () {
+	#echo "Param1: $1"
+	orig_path=$(echo $1 | sed  's/^\.//')
+	echo $orig_path
+	diff_result=$(diff $orig_path $1)
+	#echo "Diff result: $diff_result"
+	if [ -z "$diff_result" ];
+	then
+	  true
+	else
+	  false
+	fi
+}
+
+function verify_directories () {
+    echo "$1" | while read -r p ;
+    do
+       find "$p" -type f -exec bash -c 'is_equal "{}" && echo "yes" || echo "no"' \;
+    done
+}
+
+export -f is_equal
+
+
 echo "Syncing config files from System to Git repository"
 
-find ./boot -type f -exec echo "syncing ... " {} \;
+
+verify_directories "./boot"
+verify_directories "./etc"
+verify_directories "./usr/local"
