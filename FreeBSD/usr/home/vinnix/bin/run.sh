963d330fec7fcaa50db2ca77d792062f43af3486
Vinícius Schmidt <vinicius.schmidt@gmail.com>
2019-02-09 10:23:27 -0200

    Last update: kernel,world and packages

--- FreeBSD/usr/home/vinnix/bin/run.sh
+++ FreeBSD/usr/home/vinnix/bin/run.sh
@@ -11,3 +11,4 @@ pkg query -e '%m = ports@FreeBSD.org' %o > pkg.unmaintained.output
 cat analysis.autoremove.output | grep " 0 dep" | grep "0 that" | awk '{print $4}' | sort | uniq
 cat analysis.leaf.output | grep " 0 dep" | grep "0 that" | awk '{print $4}' | sort | uniq
 
+uname -a > kernel.version.output
