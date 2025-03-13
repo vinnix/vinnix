d0e53f250b3dd5a8025cdfd50ce783de3a580db1
Vinícius Schmidt <vinnix.bsd@gmail.com>
2025-03-13 05:06:53 +0000

    Adding reference

--- misc_util/git_rm.sh
+++ misc_util/git_rm.sh
@@ -1,2 +1,5 @@
+# Useful trick for handling GIT and filtering security breaches
+# For example keys leaking on repository
+
 git filter-branch --index-filter 'git rm -rf --cached --ignore-unmatch path_to_file' HEAD
 
